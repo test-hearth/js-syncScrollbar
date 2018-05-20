20cbe1517811b1898b4510835542e7dc289b224b
keyhearth <keyhearth2233@gmail.com>
2018-05-21 01:06:17 +0900

    scrollのsync完了

--- main.js
+++ main.js
@@ -1,3 +1,19 @@
 // window.onload = (() => {
 //     $('body #page-body').text('onload js complited!!');
 // })
+
+window.onload = (() => {
+  const min = 894;
+  const max = 2548;
+  let rdmWidth = Math.floor( Math.random() * ( max + 1 - min)) + min;
+  $('#entity-containe').width(rdmWidth);
+
+  $('div#virtual-scroll>div').width(rdmWidth);
+});
+
+// スクロール連動
+function scrLinkage(ids){
+  let j = jQuery.noConflict();
+	st = j('#'+ids).scrollLeft();
+	j('.linkage').scrollLeft(st);
+}
